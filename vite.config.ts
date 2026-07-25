09bee2aacb79edfd95b2c22ca7a1ab6c8c34fa9c
Devendra Agnihotri <devendra.bhumca12@gmail.com>
2026-07-25 15:49:52 +0530

    fix: set correct base path for project site

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/devendra.github.io/',
   plugins: [react(), tailwindcss()],
 })
